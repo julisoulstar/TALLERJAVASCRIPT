fbb3e2a60e2b992b5d4c0ccc66c24703ead16cc1
ErikaPrins <ErikaPrins@10.72.133.33>
2017-07-20 12:44:50 -0500

    punto 14, sin problemas

---
+++ public_html/p14/js/p14.js
@@ -0,0 +1,10 @@
+    var number =  prompt('Enter the number of employees in your company');
+    money=258320000/parseInt(number);
+    console.log("Your company has : $258.320.000");
+    console.log("Each employee will receive : $" + money);
+
+    var hours =  prompt('How many hours do you work per day?');
+    gain=45000*parseInt(hours)-20000;
+    console.log("Your profit today is $" + gain);
+
+    console.log("There are " + 2.54*12*5280 + " centimeters in a mile");
