516a96882acf171430b94bb0cc71a12b64ffd563
ErikaPrins <ErikaPrins@10.72.133.33>
2017-07-19 22:45:15 -0500

    punto 8, minguna dificultad

---
+++ public_html/p8/js/p8.js
@@ -0,0 +1,10 @@
+    var number1 = prompt('Enter a number');
+    var number2 = prompt('Enter a number');
+    console.log("the adition between "+ number1 + " and "+ number2 + " is: ");
+    console.log(parseInt(number1)+parseInt(number2));
+    console.log("The subtraction between "+ number1 + " and "+ number2 + " is: ");
+    console.log(parseInt(number1)-parseInt(number2));
+    console.log("The multiplication between "+ number1 + " and "+ number2 + " is: ");
+    console.log(parseInt(number1)*parseInt(number2));
+    console.log("The division between "+ number1 + " and "+ number2 + " is: ");
+    console.log(parseInt(number1)/parseInt(number2));
