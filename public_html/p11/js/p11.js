70a7d4fc205974054aa1ceb24f6244947f8cc616
ErikaPrins <ErikaPrins@10.72.133.33>
2017-07-20 11:16:25 -0500

    punto 11, hubo una duda que fue resuelta

---
+++ public_html/p11/js/p11.js
@@ -0,0 +1,11 @@
+    var asterisk1 = "";
+    var asterisk = "*", i = 0;
+    do {
+      asterisk1=asterisk1+asterisk;
+      i++;
+      console.log(asterisk1);
+    } while (i <= 5);
+    
+    if (asterisk1 === "******" ) {
+    var i = 5;
+    }
