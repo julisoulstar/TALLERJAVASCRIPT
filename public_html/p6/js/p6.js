26540d3a5c43f127017a838f392ff2e4f4492f20
ErikaPrins <ErikaPrins@10.72.133.33>
2017-07-19 22:07:53 -0500

    punto 6, minguna dificultad

---
+++ public_html/p6/js/p6.js
@@ -0,0 +1,17 @@
+
+    console.log("50 to 10 is:");
+    console.log(parseInt(50)**parseInt(10));
+    console.log("10 to 50 is:");
+    console.log(parseInt(10)**parseInt(50));
+    console.log("The multiplication between 50 and 10 is:");
+    console.log(parseInt(50)*parseInt(10));
+    console.log("The division between 50 and 10 is:");
+    console.log(parseInt(50)/parseInt(10));
+    console.log("The division between 10 and 50 is:");
+    console.log(parseInt(10)/parseInt(50));
+    console.log("The module of the division between 50 and 10 is:");
+    console.log(parseInt(50)%parseInt(10));
+    console.log("The subtraction between 50 and 10 is:");
+    console.log(parseInt(50)-parseInt(10));
+    console.log("The adition between 50 and 10 is:");
+    console.log(parseInt(50)+parseInt(10));
