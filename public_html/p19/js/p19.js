04438695eef2cb8d01106c6abb73944db299f45c
ErikaPrins <ErikaPrins@10.72.133.33>
2017-07-22 17:19:33 -0500

    punto 19, ninguna dificultad

---
+++ public_html/p19/js/p19.js
@@ -0,0 +1,7 @@
+    var number = prompt(" enter a whole number ");
+	if (number<=0){
+	number2=parseInt(number)*(-1);
+	console.log(number2);
+    }else{
+	console.log(" you must enter a number less than zero ");
+    }
