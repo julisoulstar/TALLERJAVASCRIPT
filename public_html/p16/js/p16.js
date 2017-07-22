474e059c3e911f8a5c84aaf5f98fa98bfdd02fc4
ErikaPrins <ErikaPrins@10.72.133.33>
2017-07-22 17:16:39 -0500

    punto 16, ninguna dificultad

---
+++ public_html/p16/js/p16.js
@@ -0,0 +1,6 @@
+    var height = prompt("write the measure of the height of the rectangle");
+    var base = prompt("write the measure of the base of the rectangle");
+    perimeter=(2*parseInt(height))+(2*parseInt(base));
+	area=parseInt(height)*parseInt(base);
+    console.log("the perimeter of the rectangle is : " + perimeter);
+    console.log("the area of the rectangle is : " + area);
