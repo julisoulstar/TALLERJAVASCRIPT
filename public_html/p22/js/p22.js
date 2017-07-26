c88a1cd33ee5d0f3e8e023da0cd03e545825e4d5
ErikaPrins <ErikaPrins@10.72.133.33>
2017-07-25 22:14:25 -0500

    punto 22, sin dificultades

---
+++ public_html/p22/js/p22.js
@@ -0,0 +1,17 @@
+        year=15;
+        console.log(" 15 years posseses : ");
+        month=year*12;
+        day=year*365;
+        hour=year*(24*365);
+        minute=year*(60*(24*365));
+        second=year*(60*(60*(24*365)));
+
+        console.log(" months : "+month);
+
+        console.log(" days : "+day);
+
+        console.log(" hours : "+hour);
+
+        console.log(" minutes : "+minute);
+
+        console.log(" seconds : "+second);
