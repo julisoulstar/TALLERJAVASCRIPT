2669ef6a7fd438de5cdd28e18f68e9714997a5f0
ErikaPrins <ErikaPrins@10.72.133.33>
2017-07-26 09:11:44 -0500

    punto 24, sin dificultades

---
+++ public_html/p24/js/p24.js
@@ -0,0 +1,7 @@
+      var hours = parseInt(prompt(" enter hours "));
+
+      week=hours/(24*7);
+      day=hours/24;
+      console.log(" it represents ");
+      console.log(" weeks : "+week);
+      console.log(" days : "+day);
