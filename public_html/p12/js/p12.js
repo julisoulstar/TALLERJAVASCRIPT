466f1936320b69f633d0502cc9bfa0c94d8536f2
ErikaPrins <ErikaPrins@10.72.133.33>
2017-07-29 20:38:29 -0500

    punto 12, se termino con exito

--- public_html/p12/js/p12.js
+++ public_html/p12/js/p12.js
@@ -1,10 +1,19 @@
-    var asterisk1 = "";
-    var asterisk = "*", i = 0;
-    while (i <= 5) {
-    i++;
-    asterisk1=asterisk1+asterisk;
-    console.log(asterisk1);
-    }
-    if (asterisk1 === "******" ) {
-    var i = 6;
+    var a = "*";
+    for (var i=1;i<=13;i++) {
+      if (i === 1 || i === 13) {
+        console.log('    ' + a);
+      }
+      if (i === 2 || i === 12 || i === 5 || i === 6) {
+        console.log('   ' + a + a + a);
+      }
+      if (i === 3 || i === 7 || i === 11) {
+        console.log('  ' + a + a + a + a + a );
+      }
+      if (i === 4 || i === 8 || i === 10) {
+        console.log(' ' + a + a + a + a + a + a + a );
+      }
+      if (i === 9) {
+        console.log( a + a + a + a + a + a + a + a + a );
+      }
+
     }
