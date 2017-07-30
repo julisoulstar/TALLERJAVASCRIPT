a94d62f3eb200e624bf667d464ed13d9d3fc8862
ErikaPrins <ErikaPrins@10.72.133.33>
2017-07-29 19:57:35 -0500

    punto 23, se tenian dudas acerca del objeto Date, pero se resolvio

---
+++ public_html/p23/js/p23.js
@@ -0,0 +1,18 @@
+    var date =  prompt('Enter first date YY , MM , DD');
+    console.log('First date entered : '+date);
+    
+    var date2 =  prompt('Enter second date YY , MM , DD');
+    console.log('Second date entered : '+date2);
+    
+    
+    
+    var a = new Date (date);
+    var b = new Date (date2);
+    
+    var c =a-b;
+    var days = Math.floor(c/(1000*60*60*24));
+    months=days/30;
+    years=months/12;
+    console.log('Difference in days : '+days);
+    console.log('Difference in months : '+months);
+    console.log('Difference in years : '+years);
