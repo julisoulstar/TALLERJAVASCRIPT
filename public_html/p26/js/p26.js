df97568a14bd3819dff5805c9ce8d8a3831a744b
ErikaPrins <ErikaPrins@10.72.133.33>
2017-07-31 17:16:04 -0500

    punto 26, terminado

--- public_html/p26/js/p26.js
+++ public_html/p26/js/p26.js
@@ -1,14 +1,49 @@
-    var opcion = prompt("   1. Realizar una suma\n\
+
+function menu() {
+  var option = prompt("   1. Realizar una suma\n\
     2. Realizar una resta\n\
     3. Realizar una multiplicacion\n\
     4. Realizar una division\n\
     0. Salir del programa");
-      if (opcion == 1) {
-        var number1 = parseInt(prompt('Enter a number'));
-        var number2 = parseInt(prompt('Enter another number'));
-        function adition (number1, number2) {
-          var result = parseInt(number1)+parseInt(number2);
-          return result;
-    console.log('The result of the sum is : '+result);
-        }
-    }else{}
+  return parseInt(option);
+}
+
+var result = menu();
+function adition() {
+  var number1 = parseInt(prompt('Enter a number'));
+  var number2 = parseInt(prompt('Enter another number'));
+  console.log(`The result of the sum is : ${ number1 + number2}`);
+}
+function subtraction() {
+  var number1 = parseInt(prompt('Enter a number'));
+  var number2 = parseInt(prompt('Enter another number'));
+  console.log(`The result of the subtraction is : ${ number1 - number2}`);
+}
+function multiplication() {
+  var number1 = parseInt(prompt('Enter a number'));
+  var number2 = parseInt(prompt('Enter another number'));
+  console.log(`The multiplication result is : ${ number1 * number2}`);
+}
+function division() {
+  var number1 = parseInt(prompt('Enter a number'));
+  var number2 = parseInt(prompt('Enter another number'));
+  console.log(`The result of the division is : ${ number1 / number2}`);
+}
+switch (result) {
+  case 1:
+    adition();
+    break;
+  case 2:
+    subtraction();
+    break;
+  case 3:
+    multiplication();
+    break;
+  case 4:
+    division();
+    break;
+  default:
+    console.log('adios');
+    break;
+}
+
