c6783f404523c5265f35429f077ccd1ee0068900
ErikaPrins <ErikaPrins@10.72.133.33>
2017-10-01 15:06:03 -0500

    listo

--- public_html/p59/js/p59.js
+++ public_html/p59/js/p59.js
@@ -1,16 +1,13 @@
 function math() {
-  var date1 = new Date($("#date1")).val();
-  var date2 = new Date($("#date2")).val();
+  var date1 = new Date($("#date1").val());
+  var date2 = new Date($("#date2").val());
   if (date2.getTime() > date1.getTime()) {
     var diference = date2.getTime() - date1.getTime();
   } else {
     var diference = date1.getTime() - date2.getTime();
   }
-  var array = [];
-  array[0] = Math.floor(diference / (1000 * 60 * 60 * (24 * 364)));
-  array[1] = Math.floor(diference / (1000 * 60 * 60 * (24 * 30)));
-  array[2] = Math.floor(diference / (1000 * 60 * 60 * 24));
-  return diference;
-
-  $("#text").text(array[0] + "" + array[1] + "" + array[2]);
+  var years = parseInt(diference / (1000 * 60 * 60 * (24 * 364)));
+  var months = parseInt(diference / (1000 * 60 * 60 * (24 * 30.4167)));
+  var days = parseInt(diference / (1000 * 60 * 60 * 24));
+  $("#text").text("difference in years : " + years + " difference in months : " + months + " difference in days : " + days);
 }
